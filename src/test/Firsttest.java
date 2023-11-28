47515df54d3f781694ad90a16cd8e9745f5ec684
ManjiriGosavi <ManjiriGosavi@FU-CORP002554>
2023-11-28 14:27:14 +0530

    First commit

---
+++ src/test/Firsttest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Firsttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Test");
+System.out.println("Hi");
+	}
+
+}
