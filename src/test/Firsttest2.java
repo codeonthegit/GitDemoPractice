3c9990237b07301400afad5cce508397fdea012c
ManjiriGosavi <ManjiriGosavi@FU-CORP002554>
2023-12-04 16:01:51 +0530

    Again second

--- src/test/Firsttest2.java
+++ src/test/Firsttest2.java
@@ -5,7 +5,7 @@ public class Firsttest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Test 2");
-System.out.println("Hi");
+System.out.println("Hi1");
 	}
 
 }
