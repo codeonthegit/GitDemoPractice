41f3ab3abd4341b1438f21597bf8f19c8f62a3a0
ManjiriGosavi <ManjiriGosavi@FU-CORP002554>
2023-12-04 15:54:05 +0530

    commit and push this Firstest2

---
+++ src/test/Firsttest2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Firsttest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Test 2");
+System.out.println("Hi");
+	}
+
+}
